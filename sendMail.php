ef002b2f8ff66e47ffa5250c8b1510a7792aa929
Jaico Nakpil <jaiconakpil@jaicos-mbp.towson.edu>
2019-12-03 15:06:26 -0500

    emailChangespt2

--- sendMail.php
+++ sendMail.php
@@ -2,12 +2,25 @@
 $subject = 'Bill Report';
 $emailTo = $_POST['email'];
 echo $emailTo;
-$billOne = $_POST['nameBilling'] + $_POST['billResult1'];
-$billTwo = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill1 = $_POST['nameBilling'] + $_POST['billResult1'];
+$bill2 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill3 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill4 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill5 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill6 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill7 = $_POST['nameBilling2'] + $_POST['billResult2'];
+$bill8 = $_POST['nameBilling2'] + $_POST['billResult2'];
 $paypalLink= $_POST['unPaypal'];
 
-
-$message .= 'Bill 1: '.$billOne. + " Bill 2: "  .$billTwo. " ";
+$message .='Bills shown are divided by the amount of roomates';
+$message .= 'Bill 1: '.$bill1;
+$message .= 'Bill 2: '.$bill2;
+$message .= 'Bill 3: '.$bill3;
+$message .= 'Bill 4: '.$bill4;
+$message .= 'Bill 5: '.$bill5;
+$message .= 'Bill 6: '.$bill6;
+$message .= 'Bill 7: '.$bill7;
+$message .= 'Bill 8: '.$bill8;
 $message .= 'Link to pay ' .$paypalLink;
 
 
