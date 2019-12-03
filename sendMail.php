57aa3f4cba96db43eec40fc8ca089a936dd8b881
Jaico Nakpil <jaiconakpil@jaicos-mbp.towson.edu>
2019-12-03 14:44:07 -0500

    emaillink

--- sendMail.php
+++ sendMail.php
@@ -2,8 +2,13 @@
 $subject = 'Bill Report';
 $emailTo = $_POST['email'];
 echo $emailTo;
+$billOne = $_POST['nameBilling'] + $_POST['billResult1'];
+$billTwo = $_POST['nameBilling'] + $_POST['billResult1'];
+$paypalLink= $_POST['unPaypal'];
 
-$message .= $_POST['nameBilling1'] + $_POST['billResult1'];
+
+$message .= 'Bill 1: '.$billOne. + "" .$billTwo. "";
+$message .= 'Link to pay' .$paypalLink;
 
 
 mail($emailTo, $subject, $message);
